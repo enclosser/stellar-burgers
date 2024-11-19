28d70f9409826e06f419d55bc1fa650483941c2c
andrei.antonov <andrei.antonov@corp.mail.ru>
2024-11-19 14:48:56 +0300

    Доработан header и добавлены маршруты

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -14,7 +14,7 @@ import '../../index.css';
 import styles from './app.module.css';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
-import { AppHeader } from '@components';
+import { AppHeader, Modal, OrderInfo, IngredientDetails } from '@components';
 
 const App = () => {
   const location = useLocation();
@@ -26,6 +26,32 @@ const App = () => {
       <Routes location={background || location}>
         <Route path='/' element={<ConstructorPage />} />
         <Route path='/feed' element={<Feed />} />
+        <Route
+          path='/feed/:id'
+          element={
+            <div>
+              <p
+                className={`text text_type_main-medium ${styles.detailHeader}`}
+              >
+                {`#${location.pathname.match(/\d+/)}`}
+              </p>
+              <OrderInfo />
+            </div>
+          }
+        />
+        <Route
+          path='/ingredients/:id'
+          element={
+            <div>
+              <p
+                className={`text text_type_main-medium ${styles.detailHeader}`}
+              >
+                Детали ингредиента
+              </p>
+              <IngredientDetails />
+            </div>
+          }
+        />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
+import classNames from 'classnames';
 import styles from './app-header.module.css';
 import { TAppHeaderUIProps } from './type';
 import {
@@ -7,28 +8,60 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
+import { NavLink } from 'react-router-dom';
 
+// Компонент для отображения навигационного элемента
+const NavItem: FC<{
+  to: string;
+  icon: React.ElementType;
+  label: string;
+  extraClass?: string;
+}> = ({ to, icon: Icon, label, extraClass = '' }) => (
+  <NavLink
+    to={to}
+    className={({ isActive }) =>
+      classNames(styles.link, extraClass, { [styles.link_active]: isActive })
+    }
+  >
+    {({ isActive }) => (
+      <>
+        <Icon type={isActive ? 'primary' : 'secondary'} />
+        <p className='text text_type_main-default ml-2'>{label}</p>
+      </>
+    )}
+  </NavLink>
+);
+
+// Основной компонент AppHeaderUI
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
-    <nav className={`${styles.menu} p-4`}>
+    <nav className={classNames(styles.menu, 'p-4')}>
       <div className={styles.menu_part_left}>
-        <>
-          <BurgerIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </>
-        <>
-          <ListIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </>
+        <NavItem
+          to='/'
+          icon={BurgerIcon}
+          label='Конструктор'
+          extraClass='ml-10 mr-10'
+        />
+        <NavItem
+          to='/feed'
+          icon={ListIcon}
+          label='Лента заказов'
+          extraClass='ml-10'
+        />
       </div>
       <div className={styles.logo}>
-        <Logo className='' />
+        <NavLink to='/'>
+          <Logo className='' />
+        </NavLink>
       </div>
       <div className={styles.link_position_last}>
-        <ProfileIcon type={'primary'} />
-        <p className='text text_type_main-default ml-2'>
-          {userName || 'Личный кабинет'}
-        </p>
+        <NavItem
+          to='/profile'
+          icon={ProfileIcon}
+          label={userName || 'Личный кабинет'}
+          extraClass='ml-10'
+        />
       </div>
     </nav>
   </header>
