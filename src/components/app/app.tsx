d6f709a31b722ad923d7ba1754d71c906fd197fa
andrei.antonov <andrei.antonov@corp.mail.ru>
2024-11-20 00:10:05 +0300

    Написаны слайсы и переботаны компоненты для отображения конструктора и добавления ингридиентов

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,64 +1,28 @@
-import {
-  ConstructorPage,
-  Feed,
-  ForgotPassword,
-  Login,
-  NotFound404,
-  Profile,
-  ProfileOrders,
-  Register,
-  ResetPassword
-} from '@pages';
-
+import { ConstructorPage, Feed } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
-import { AppHeader, Modal, OrderInfo, IngredientDetails } from '@components';
+import { AppHeader } from '@components';
+import { useEffect } from 'react';
+import { useDispatch } from '../../services/store';
+import { getIngredientsThunk } from '../../services/slices/burger-ingredients';
 
 const App = () => {
   const location = useLocation();
-  const background = location.state?.background;
+  const backgroundLocation = location.state?.background;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getIngredientsThunk());
+  }, [dispatch]);
 
   return (
     <div className={styles.app}>
       <AppHeader />
-      <Routes location={background || location}>
+      <Routes location={backgroundLocation || location}>
         <Route path='/' element={<ConstructorPage />} />
         <Route path='/feed' element={<Feed />} />
-        <Route
-          path='/feed/:id'
-          element={
-            <div>
-              <p
-                className={`text text_type_main-medium ${styles.detailHeader}`}
-              >
-                {`#${location.pathname.match(/\d+/)}`}
-              </p>
-              <OrderInfo />
-            </div>
-          }
-        />
-        <Route
-          path='/ingredients/:id'
-          element={
-            <div>
-              <p
-                className={`text text_type_main-medium ${styles.detailHeader}`}
-              >
-                Детали ингредиента
-              </p>
-              <IngredientDetails />
-            </div>
-          }
-        />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/reset-password' element={<ResetPassword />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/profile/orders' element={<ProfileOrders />} />
-        <Route path='/*' element={<NotFound404 />} />
       </Routes>
     </div>
   );
--- src/components/ingredients-category/ingredients-category.tsx
+++ src/components/ingredients-category/ingredients-category.tsx
@@ -1,30 +1,29 @@
 import { forwardRef, useMemo } from 'react';
 import { TIngredientsCategoryProps } from './type';
-import { TIngredient } from '@utils-types';
-import { IngredientsCategoryUI } from '../ui/ingredients-category';
+import { IngredientsCategoryUI } from '@ui';
+import { useSelector } from '../../services/store';
+import { getConstructorState } from '../../services/slices/burger-constructor';
 
 export const IngredientsCategory = forwardRef<
   HTMLUListElement,
   TIngredientsCategoryProps
 >(({ title, titleRef, ingredients }, ref) => {
-  /** TODO: взять переменную из стора */
-  const burgerConstructor = {
-    bun: {
-      _id: ''
-    },
-    ingredients: []
-  };
+  const { bun, ingredients: constructorIngredients } =
+    useSelector(getConstructorState);
 
   const ingredientsCounters = useMemo(() => {
-    const { bun, ingredients } = burgerConstructor;
-    const counters: { [key: string]: number } = {};
-    ingredients.forEach((ingredient: TIngredient) => {
-      if (!counters[ingredient._id]) counters[ingredient._id] = 0;
-      counters[ingredient._id]++;
-    });
+    const counters = constructorIngredients.reduce(
+      (acc, { _id }) => {
+        acc[_id] = (acc[_id] || 0) + 1;
+        return acc;
+      },
+      {} as Record<string, number>
+    );
+
     if (bun) counters[bun._id] = 2;
+
     return counters;
-  }, [burgerConstructor]);
+  }, [bun, constructorIngredients]);
 
   return (
     <IngredientsCategoryUI
