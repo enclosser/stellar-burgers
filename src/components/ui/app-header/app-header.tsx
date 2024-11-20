3405c3ec62c676a857d6be9767269ca7de2ff0a8
andrei.antonov <andrei.antonov@corp.mail.ru>
2024-11-20 16:20:46 +0300

    Доработана авторизация и отображение истории заказов пользователя

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -1,5 +1,4 @@
-import { FC } from 'react';
-import classNames from 'classnames';
+import React, { FC } from 'react';
 import styles from './app-header.module.css';
 import { TAppHeaderUIProps } from './type';
 import {
@@ -10,59 +9,61 @@ import {
 } from '@zlden/react-developer-burger-ui-components';
 import { NavLink } from 'react-router-dom';
 
-// Компонент для отображения навигационного элемента
-const NavItem: FC<{
-  to: string;
-  icon: React.ElementType;
-  label: string;
-  extraClass?: string;
-}> = ({ to, icon: Icon, label, extraClass = '' }) => (
-  <NavLink
-    to={to}
-    className={({ isActive }) =>
-      classNames(styles.link, extraClass, { [styles.link_active]: isActive })
+export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
+  const menuItems = [
+    {
+      to: '/',
+      icon: <BurgerIcon type='primary' />,
+      text: 'Конструктор',
+      extraClass: 'mr-10'
+    },
+    {
+      to: '/feed',
+      icon: <ListIcon type='primary' />,
+      text: 'Лента заказов'
     }
-  >
-    {({ isActive }) => (
-      <>
-        <Icon type={isActive ? 'primary' : 'secondary'} />
-        <p className='text text_type_main-default ml-2'>{label}</p>
-      </>
-    )}
-  </NavLink>
-);
+  ];
 
-// Основной компонент AppHeaderUI
-export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
-  <header className={styles.header}>
-    <nav className={classNames(styles.menu, 'p-4')}>
-      <div className={styles.menu_part_left}>
-        <NavItem
-          to='/'
-          icon={BurgerIcon}
-          label='Конструктор'
-          extraClass='ml-10 mr-10'
-        />
-        <NavItem
-          to='/feed'
-          icon={ListIcon}
-          label='Лента заказов'
-          extraClass='ml-10'
-        />
-      </div>
-      <div className={styles.logo}>
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.menu} p-4`}>
+        <div className={styles.menu_part_left}>
+          {menuItems.map(({ to, icon, text, extraClass }, index) => (
+            <NavLink
+              key={index}
+              to={to}
+              className={({ isActive }) =>
+                `${styles.link} ${isActive ? styles.link_active : ''}`
+              }
+            >
+              {icon}
+              <p
+                className={`text text_type_main-default ml-2 ${extraClass || ''}`}
+              >
+                {text}
+              </p>
+            </NavLink>
+          ))}
+        </div>
         <NavLink to='/'>
-          <Logo className='' />
+          <div className={styles.logo}>
+            <Logo className='' />
+          </div>
         </NavLink>
-      </div>
-      <div className={styles.link_position_last}>
-        <NavItem
-          to='/profile'
-          icon={ProfileIcon}
-          label={userName || 'Личный кабинет'}
-          extraClass='ml-10'
-        />
-      </div>
-    </nav>
-  </header>
-);
+        <div className={styles.link_position_last}>
+          <NavLink
+            to='/profile'
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.link_active : ''}`
+            }
+          >
+            <ProfileIcon type='primary' />
+            <p className='text text_type_main-default ml-2'>
+              {userName || 'Личный кабинет'}
+            </p>
+          </NavLink>
+        </div>
+      </nav>
+    </header>
+  );
+};
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,12 +1,34 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
+import { useDispatch, useSelector } from '../../services/store';
+import { useNavigate } from 'react-router-dom';
+import {
+  getUserData,
+  getUserError,
+  loginUserThunk
+} from '../../services/slices/user';
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e: SyntheticEvent) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const error = useSelector(getUserError);
+  const user = useSelector(getUserData);
+
+  const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
+    await dispatch(
+      loginUserThunk({
+        email: email,
+        password: password
+      })
+    );
+
+    if (!error && user) {
+      navigate('/');
+    }
   };
 
   return (
