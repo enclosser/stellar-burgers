ad4c17e64ed1560c62ddee7f66aac73b66c89916
andrei.antonov <andrei.antonov@corp.mail.ru>
2024-11-20 20:13:45 +0300

    Фикс бага с постоянным редиректом и добавлено модальное окно которое при некорректных учетных данных выводит модальное окно

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,14 +3,17 @@ import { LoginUI } from '@ui-pages';
 import { useDispatch, useSelector } from '../../services/store';
 import { useNavigate } from 'react-router-dom';
 import {
-  getUserData,
   getUserError,
+  getUserData,
   loginUserThunk
 } from '../../services/slices/user';
+import { Modal } from '@components';
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalErrorText, setModalErrorText] = useState('');
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -26,19 +29,34 @@ export const Login: FC = () => {
       })
     );
 
-    if (!error && user) {
+    if (error) {
+      setModalErrorText('Некорректный логин или пароль');
+      setIsModalOpen(true);
+    } else if (user) {
       navigate('/');
     }
   };
 
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setModalErrorText('');
+  };
+
   return (
-    <LoginUI
-      errorText=''
-      email={email}
-      setEmail={setEmail}
-      password={password}
-      setPassword={setPassword}
-      handleSubmit={handleSubmit}
-    />
+    <>
+      <LoginUI
+        errorText=''
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        handleSubmit={handleSubmit}
+      />
+      {isModalOpen && (
+        <Modal title='Error' onClose={closeModal}>
+          <p>{modalErrorText}</p>
+        </Modal>
+      )}
+    </>
   );
 };
