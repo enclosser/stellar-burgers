88a575395157eb206df2774b44bfc253b4a2a02e
andrei.antonov <andrei.antonov@corp.mail.ru>
2024-12-13 04:18:36 +0300

    Подключение

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,9 +1,22 @@
-/// <reference types="cypress" />
+/// <reference types="cypress" /> # TODO Пока не удается избавится от этого
 
 describe('Тестирование конструктора бургера', () => {
   const url = 'http://localhost:4000';
 
-  it(`Проверка доступности страницы по адресу ${url}`, () => {
+  it('Страница конструктора бургера доступна', () => {
+    // Проверяем, что URL совпадает
     cy.visit(url);
+    cy.log('Страница конструктора успешно загружена.');
+  });
+
+  beforeEach(() => {
+    // Установка фиктивных данных и перехват запросов
+    cy.intercept('GET', '/api/ingredients', { fixture: 'ingredients.json' }).as(
+      'getIngredients'
+    );
+    cy.setCookie('accessToken', 'mockAccessToken');
+    localStorage.setItem('refreshToken', 'mockRefreshToken');
+    cy.visit(url);
+    cy.wait('@getIngredients');
   });
 });
