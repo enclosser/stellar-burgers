f211f6203a9b2ab6150aa37ff775aa476e456a44
andrei.antonov <andrei.antonov@corp.mail.ru>
2024-12-13 03:25:44 +0300

    Первый рабочий тест ура!

---
+++ cypress/e2e/constructor.cy.tsx
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('Тестирование конструктора бургера', () => {
+  const url = 'http://localhost:4000';
+
+  it(`Проверка доступности страницы по адресу ${url}`, () => {
+    cy.visit(url);
+  });
+});
