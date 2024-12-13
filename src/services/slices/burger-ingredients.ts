28f51919891ce76ca5f9c82787196c5059618cdf
andrei.antonov <andrei.antonov@corp.mail.ru>
2024-12-13 19:58:32 +0300

    добавлен тест burger-ingredients.ts

--- src/services/slices/burger-ingredients.ts
+++ src/services/slices/burger-ingredients.ts
@@ -16,7 +16,7 @@ type TIngredientsState = {
   error: string | null;
 };
 
-const initialState: TIngredientsState = {
+export const initialState: TIngredientsState = {
   ingredients: [],
   buns: [],
   mains: [],
