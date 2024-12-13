a2d57c01b8eadaea95c182ddd94ecda7425cebff
andrei.antonov <andrei.antonov@corp.mail.ru>
2024-12-13 13:33:45 +0300

    Тестирование оформления заказа + тестовый ордер

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -34,33 +34,76 @@ describe('Тестирование конструктора бургера', () 
     cy.log('Страница конструктора успешно загружена.');
   });
 
-  describe('Тестирование формирования заказа', () => {
-    const verifyIngredientAddition = (
-      category,
-      ingredientName,
-      verificationText
-    ) => {
-      // Проверка отображения элемента и добавление ингредиента
-      cy.get('div').contains(verificationText).should('exist');
-      cy.get('h3')
-        .contains(category)
-        .next('ul')
-        .contains(ingredientName)
-        .click();
-      cy.get('div').contains(verificationText).should('not.exist');
-      cy.log(`${ingredientName} из категории "${category}" успешно добавлен.`);
-    };
+  describe('Тестирование создания заказа', () => {
+    describe('Тестирование добавления ингредиентов', () => {
+      const verifyIngredientAddition = (
+        category,
+        ingredientName,
+        verificationText
+      ) => {
+        cy.contains(verificationText).should('exist');
+        cy.get('h3')
+          .contains(category)
+          .next('ul')
+          .contains(ingredientName)
+          .click();
+        cy.contains(verificationText).should('not.exist');
+        cy.log(
+          `${ingredientName} из категории "${category}" успешно добавлен.`
+        );
+      };
 
-    it('Добавление булки', () => {
-      verifyIngredientAddition('Булки', 'Добавить', 'Выберите булки');
+      it('Добавление булки', () => {
+        verifyIngredientAddition('Булки', 'Добавить', 'Выберите булки');
+      });
+
+      it('Добавление начинки', () => {
+        verifyIngredientAddition('Начинки', 'Добавить', 'Выберите начинку');
+      });
+
+      it('Добавление соуса', () => {
+        verifyIngredientAddition('Соусы', 'Добавить', 'Выберите начинку');
+      });
     });
 
-    it('Добавление начинки', () => {
-      verifyIngredientAddition('Начинки', 'Добавить', 'Выберите начинку');
-    });
+    describe('Тестирование оформления заказа', () => {
+      it('Оформление заказа', () => {
+        cy.intercept('POST', '/api/orders', { fixture: 'order.json' }).as(
+          'postOrders'
+        );
 
-    it('Добавление соуса', () => {
-      verifyIngredientAddition('Соусы', 'Добавить', 'Выберите начинку');
+        // Проверка что модальное окно не активно
+        cy.get('[data-cy=modal]').should('not.exist');
+
+        ['Булки', 'Начинки', 'Соусы'].forEach((category) => {
+          cy.get('h3')
+            .contains(category)
+            .next('ul')
+            .contains('Добавить')
+            .click();
+        });
+
+        cy.contains('Оформить заказ').click();
+        cy.wait('@postOrders');
+
+        // Проверка что цена соответствует корректной
+        cy.contains('3024').should('exist');
+
+        // Проверка отображения модального окна и номера заказа
+        cy.get('[data-cy=modal]').should('be.visible');
+        cy.contains('11').should('exist');
+
+        // Проверка закрытия модального окна и отсутствия номера заказа
+        cy.get('[data-cy=close-button]').click();
+        cy.get('[data-cy=modal]').should('not.exist');
+        cy.contains('11').should('not.exist');
+        cy.contains('3024').should('not.exist');
+
+        // Проверка контента после очистки
+        ['Выберите булки', 'Выберите начинку'].forEach((text) => {
+          cy.contains(text).should('exist');
+        });
+      });
     });
   });
 });
